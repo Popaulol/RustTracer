e11dd2c7ffbef9fa68f54844dbbe9168af608f35
Strawby <staubfingercodinggames@gmail.com>
2023-12-09 19:22:05 +0100

    Fix Infinite recursion

--- src/color.rs
+++ src/color.rs
@@ -87,7 +87,7 @@ impl ops::Mul<Color> for f64 {
     type Output = Color;
 
     fn mul(self, rhs: Color) -> Self::Output {
-        self * rhs
+        Self::Output::new(self.r() * rhs, self.g() * rhs, self.b() * rhs)
     }
 }
 
